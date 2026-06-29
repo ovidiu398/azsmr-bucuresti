61673a876a14eb27e868bba6bd018365918e5955
[dyad] <git@dyad.sh>
2026-06-30 01:25:09 +0300

    [dyad] Implementarea unui sistem complet de traducere i18n cu context și dicționar pentru toate limbile. - wrote 4 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-"use client";
-
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './context/LanguageContext';
 import Index from './pages/Index';
-import BibleLessons from './pages/BibleLessons';
-import NotFound from './pages/NotFound';
-import { Toaster } from "@/components/ui/sonner";
 
 function App() {
   return (
     <LanguageProvider>
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/lectii-biblice" element={<BibleLessons />} />
-          <Route path="*" element={<NotFound />} />
         </Routes>
-        <Toaster position="top-center" />
-      </BrowserRouter>
+      </Router>
     </LanguageProvider>
   );
 }
